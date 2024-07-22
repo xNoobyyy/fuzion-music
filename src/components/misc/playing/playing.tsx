f177b08e3ecbdb964fcab40321d35e947c7e1c54
Leander Riefel <leanderriefel@gmx.de>
2024-07-22 16:52:24 +0200

    chore: Update window width in tauri.conf.json to 1200

--- src/components/misc/playing/playing.tsx
+++ src/components/misc/playing/playing.tsx
@@ -32,7 +32,7 @@ import {
   ContextMenuTrigger,
 } from "~/components/ui/context-menu"
 
-type SavedConfig = Omit<StoreType, "playing"> & {
+type SavedConfig = Omit<StoreType, "playing" | "paused"> & {
   playing: string
 }
 
@@ -67,7 +67,7 @@ export const Playing: FC = () => {
       const state = {
         ...store.state,
         playing: playing?.id,
-      }
+      } as SavedConfig
 
       await fs.writeTextFile("config.json", JSON.stringify(state), {
         dir: fs.BaseDirectory.AppConfig,
@@ -78,15 +78,8 @@ export const Playing: FC = () => {
     updateConfig()
   }, [playing, paused, shuffle, loop, volume, darkMode])
 
-  const firstRenderSongs = useRef(true)
-
   useEffect(() => {
     if (!songsQuery.isSuccess) return
-
-    if (!firstRenderSongs.current) {
-      firstRenderSongs.current = false
-      return
-    }
 
     songsStore.setState(() => songsQuery.data!)
 
@@ -113,6 +106,7 @@ export const Playing: FC = () => {
         const newState = {
           ...config,
           playing: songsQuery.data.find((value) => value.id === config.playing),
+          paused: true,
         }
 
         store.setState(() => newState)
@@ -125,27 +119,44 @@ export const Playing: FC = () => {
   const transitions = useTransitions()
 
   useEffect(() => {
-    if (!audio.current) return
+    if (!audio.current || !playing) return
 
-    if (!playing) return
+    audio.current.src = tauri.convertFileSrc(playing.file)
 
-    audio.current!.src = tauri.convertFileSrc(playing.file)
     if (paused) {
       void audio.current.pause()
     } else {
       void audio.current.play()
     }
-    store.setState((state) => ({
-      ...state,
-      currentTime: 0,
-    }))
   }, [playing])
+
+  useEffect(() => {
+    if (!audio.current || !playing) return
+
+    audio.current.src = tauri.convertFileSrc(playing.file)
+
+    if (!paused) {
+      void audio.current.play()
+    } else {
+      void audio.current.pause()
+    }
+  }, [audio.current])
 
   useEffect(() => {
     if (!audio.current) return
 
     audio.current.volume = volume / 1000
-  }, [volume])
+  }, [volume, audio.current])
+
+  useEffect(() => {
+    if (!audio.current) return
+
+    if (paused) {
+      void audio.current.pause()
+    } else {
+      void audio.current.play()
+    }
+  }, [paused, audio.current])
 
   useEffect(() => {
     transitions.disable()
@@ -188,24 +199,34 @@ export const Playing: FC = () => {
         const currentIndex = songsStore.state.findIndex(
           (value) => value.id === playing!.id
         )
-
-        if (currentIndex === songsStore.state.length - 1) {
+        if (shuffle) {
           store.setState((state) => ({
             ...state,
-            playing: songsStore.state.at(0),
+            playing: getShuffledSong(currentIndex),
             currentTime: 0,
             paused: false,
           }))
+          audio.current.currentTime = 0
+          void audio.current.play()
         } else {
-          store.setState((state) => ({
-            ...state,
-            playing: songsStore.state.at(currentIndex + 1),
-            currentTime: 0,
-            paused: false,
-          }))
+          if (currentIndex === songsStore.state.length - 1) {
+            store.setState((state) => ({
+              ...state,
+              playing: songsStore.state.at(0),
+              currentTime: 0,
+              paused: false,
+            }))
+          } else {
+            store.setState((state) => ({
+              ...state,
+              playing: songsStore.state.at(currentIndex + 1),
+              currentTime: 0,
+              paused: false,
+            }))
+          }
+          audio.current.currentTime = 0
+          void audio.current.play()
         }
-        audio.current.currentTime = 0
-        void audio.current.play()
         break
       }
     }
@@ -224,11 +245,24 @@ export const Playing: FC = () => {
   return (
     <>
       <audio
-        onPause={() => store.setState((state) => ({ ...state, paused: true }))}
-        onPlay={() => store.setState((state) => ({ ...state, paused: false }))}
-        onEnded={handleEnded}
-        onTimeUpdate={handleTimeUpdate}
+        onPause={(e) => {
+          e.preventDefault()
+          store.setState((state) => ({ ...state, paused: true }))
+        }}
+        onPlay={(e) => {
+          e.preventDefault()
+          store.setState((state) => ({ ...state, paused: false }))
+        }}
+        onEnded={(e) => {
+          e.preventDefault()
+          handleEnded()
+        }}
+        onTimeUpdate={(e) => {
+          e.preventDefault()
+          handleTimeUpdate()
+        }}
         onVolumeChange={(e) => {
+          e.preventDefault()
           if (e.currentTarget.volume < 0) {
             e.currentTarget.volume = 0
             store.setState((state) => ({
@@ -252,177 +286,187 @@ export const Playing: FC = () => {
       />
       <div className="size-full grid grid-cols-[1fr,2fr,1fr]">
         <div className="size-full relative flex items-center">
-          <Gradient url={tauri.convertFileSrc(playing.thumbnail)} />
-          <div className="ml-5 h-16 flex gap-x-4">
-            <img
-              src={tauri.convertFileSrc(playing.thumbnail)}
-              alt={playing.title}
-              className="shadow-2xl shadow-background size-16 rounded-lg object-cover"
-            />
-            <div className="flex flex-col justify-center mt-1">
-              <p className="font-bold text-sm line-clamp-1">{playing.title}</p>
-              <p className="text-sm line-clamp-1">{playing.artist}</p>
-            </div>
-          </div>
+          {playing && (
+            <>
+              <Gradient url={tauri.convertFileSrc(playing.thumbnail)} />
+              <div className="ml-5 h-16 flex gap-x-4">
+                <img
+                  src={tauri.convertFileSrc(playing.thumbnail)}
+                  alt={playing.title}
+                  className="shadow-2xl shadow-background size-16 rounded-lg object-cover"
+                />
+                <div className="flex flex-col justify-center mt-1">
+                  <p className="font-bold text-sm line-clamp-1">
+                    {playing.title}
+                  </p>
+                  <p className="text-sm line-clamp-1">{playing.artist}</p>
+                </div>
+              </div>
+            </>
+          )}
         </div>
         <div className="size-full flex flex-col justify-center gap-y-1 items-center relative overflow-hidden">
-          <div className="left-1/2 -translate-x-1/2 -z-10 bg-[radial-gradient(hsl(var(--primary)),transparent_40%)] blur-3xl inset-0 absolute" />
-          <div className="flex justify-center items-center gap-x-2">
-            <Toggle
-              size="icon"
-              onPressedChange={(toggled) =>
-                store.setState((state) => ({
-                  ...state,
-                  shuffle: toggled,
-                }))
-              }
-              pressed={shuffle}
-            >
-              <ShuffleIcon className="size-4 opacity-50" />
-            </Toggle>
-            <Button
-              size="icon"
-              variant="ghost"
-              onClick={() => {
-                store.setState((state) => ({
-                  ...state,
-                  playing: songsStore.state.at(
-                    songsStore.state.findIndex(
-                      (value) => value.id === playing.id
-                    ) - 1
-                  ),
-                  paused: false,
-                }))
-              }}
-            >
-              <TrackPreviousIcon className="size-4" />
-            </Button>
-            <Button
-              onClick={() =>
-                store.setState((state) => ({
-                  ...state,
-                  paused: !state.paused,
-                }))
-              }
-              size="icon"
-            >
-              {!paused && <PauseIcon className="size-4" />}
-              {paused && <ResumeIcon className="size-4" />}
-            </Button>
-            <Button
-              size="icon"
-              variant="ghost"
-              onClick={() => {
-                const currentIndex = songsStore.state.findIndex(
-                  (value) => value.id === playing.id
-                )
-
-                if (currentIndex === songsStore.state.length - 1) {
-                  return store.setState((state) => ({
-                    ...state,
-                    playing: songsStore.state.at(0),
-                    paused: false,
-                  }))
-                } else {
-                  store.setState((state) => ({
-                    ...state,
-                    playing: songsStore.state.at(currentIndex + 1),
-                    paused: false,
-                  }))
-                }
-              }}
-            >
-              <TrackNextIcon className="size-4" />
-            </Button>
-            <ContextMenu modal={false}>
-              <ContextMenuTrigger asChild>
+          {playing && (
+            <>
+              <div className="left-1/2 -translate-x-1/2 -z-10 bg-[radial-gradient(hsl(var(--primary)),transparent_40%)] blur-3xl inset-0 absolute" />
+              <div className="flex justify-center items-center gap-x-2">
                 <Toggle
-                  className="relative"
                   size="icon"
-                  onPressedChange={() =>
+                  onPressedChange={(toggled) =>
                     store.setState((state) => ({
                       ...state,
-                      loop:
-                        loop === "none"
-                          ? "one"
-                          : loop === "one"
-                          ? "all"
-                          : "none",
+                      shuffle: toggled,
                     }))
                   }
-                  pressed={loop === "one" || loop === "all"}
+                  pressed={shuffle}
                 >
-                  <LoopIcon className="size-4 opacity-50 relative" />
-                  {loop === "one" && (
-                    <div className="absolute flex justify-center items-center -top-1.5 -right-1.5 size-5 text-[0.65rem] leading-[0.6rem] text-center bg-primary text-primary-foreground rounded-lg">
-                      1
-                    </div>
-                  )}
-                  {loop === "all" && (
-                    <div className="absolute flex justify-center items-center -top-1.5 -right-1.5 size-5 text-[0.65rem] leading-[0.6rem] text-center bg-primary text-primary-foreground rounded-lg">
-                      ∞
-                    </div>
-                  )}
+                  <ShuffleIcon className="size-4 opacity-50" />
                 </Toggle>
-              </ContextMenuTrigger>
-              <ContextMenuContent loop className="max-w-64">
-                <ContextMenuCheckboxItem
-                  checked={loop === "none"}
-                  onCheckedChange={() =>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  onClick={() => {
                     store.setState((state) => ({
                       ...state,
-                      loop: "none",
+                      playing: songsStore.state.at(
+                        songsStore.state.findIndex(
+                          (value) => value.id === playing.id
+                        ) - 1
+                      ),
+                      paused: false,
                     }))
-                  }
+                  }}
                 >
-                  Repeat none
-                </ContextMenuCheckboxItem>
-                <ContextMenuSeparator />
-                <ContextMenuCheckboxItem
-                  checked={loop === "one"}
-                  onCheckedChange={() =>
+                  <TrackPreviousIcon className="size-4" />
+                </Button>
+                <Button
+                  onClick={() =>
                     store.setState((state) => ({
                       ...state,
-                      loop: "one",
+                      paused: !state.paused,
                     }))
                   }
+                  size="icon"
                 >
-                  Repeat one
-                </ContextMenuCheckboxItem>
-                <ContextMenuSeparator />
-                <ContextMenuCheckboxItem
-                  checked={loop === "all"}
-                  onCheckedChange={() =>
-                    store.setState((state) => ({
-                      ...state,
-                      loop: "all",
-                    }))
-                  }
+                  {!paused && <PauseIcon className="size-4" />}
+                  {paused && <ResumeIcon className="size-4" />}
+                </Button>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  onClick={() => {
+                    const currentIndex = songsStore.state.findIndex(
+                      (value) => value.id === playing.id
+                    )
+
+                    if (currentIndex === songsStore.state.length - 1) {
+                      return store.setState((state) => ({
+                        ...state,
+                        playing: songsStore.state.at(0),
+                        paused: false,
+                      }))
+                    } else {
+                      store.setState((state) => ({
+                        ...state,
+                        playing: songsStore.state.at(currentIndex + 1),
+                        paused: false,
+                      }))
+                    }
+                  }}
                 >
-                  Repeat all
-                </ContextMenuCheckboxItem>
-              </ContextMenuContent>
-            </ContextMenu>
-          </div>
-          <div className="flex justify-center items-center w-full">
-            <p>{formatTime(currentTime ?? 0)}</p>
-            <Slider
-              className="w-full mx-4 py-2"
-              value={[currentTime ?? 0]}
-              min={0}
-              max={playing.duration ?? 0}
-              onValueChange={(values) => {
-                if (audio.current) {
-                  audio.current!.currentTime = values[0]
-                  store.setState((state) => ({
-                    ...state,
-                    currentTime: values[0],
-                  }))
-                }
-              }}
-            />
-            <p>{formatTime(playing.duration ?? 0)}</p>
-          </div>
+                  <TrackNextIcon className="size-4" />
+                </Button>
+                <ContextMenu modal={false}>
+                  <ContextMenuTrigger asChild>
+                    <Toggle
+                      className="relative"
+                      size="icon"
+                      onPressedChange={() =>
+                        store.setState((state) => ({
+                          ...state,
+                          loop:
+                            loop === "none"
+                              ? "one"
+                              : loop === "one"
+                              ? "all"
+                              : "none",
+                        }))
+                      }
+                      pressed={loop === "one" || loop === "all"}
+                    >
+                      <LoopIcon className="size-4 opacity-50 relative" />
+                      {loop === "one" && (
+                        <div className="absolute flex justify-center items-center -top-1.5 -right-1.5 size-5 text-[0.65rem] leading-[0.6rem] text-center bg-primary text-primary-foreground rounded-lg">
+                          1
+                        </div>
+                      )}
+                      {loop === "all" && (
+                        <div className="absolute flex justify-center items-center -top-1.5 -right-1.5 size-5 text-[0.65rem] leading-[0.6rem] text-center bg-primary text-primary-foreground rounded-lg">
+                          ∞
+                        </div>
+                      )}
+                    </Toggle>
+                  </ContextMenuTrigger>
+                  <ContextMenuContent loop className="max-w-64">
+                    <ContextMenuCheckboxItem
+                      checked={loop === "none"}
+                      onCheckedChange={() =>
+                        store.setState((state) => ({
+                          ...state,
+                          loop: "none",
+                        }))
+                      }
+                    >
+                      Repeat none
+                    </ContextMenuCheckboxItem>
+                    <ContextMenuSeparator />
+                    <ContextMenuCheckboxItem
+                      checked={loop === "one"}
+                      onCheckedChange={() =>
+                        store.setState((state) => ({
+                          ...state,
+                          loop: "one",
+                        }))
+                      }
+                    >
+                      Repeat one
+                    </ContextMenuCheckboxItem>
+                    <ContextMenuSeparator />
+                    <ContextMenuCheckboxItem
+                      checked={loop === "all"}
+                      onCheckedChange={() =>
+                        store.setState((state) => ({
+                          ...state,
+                          loop: "all",
+                        }))
+                      }
+                    >
+                      Repeat all
+                    </ContextMenuCheckboxItem>
+                  </ContextMenuContent>
+                </ContextMenu>
+              </div>
+              <div className="flex justify-center items-center w-full">
+                <p>{formatTime(currentTime ?? 0)}</p>
+                <Slider
+                  className="w-full mx-4 py-2"
+                  value={[currentTime ?? 0]}
+                  min={0}
+                  max={playing.duration ?? 0}
+                  onValueChange={(values) => {
+                    if (audio.current) {
+                      audio.current!.currentTime = values[0]
+                      store.setState((state) => ({
+                        ...state,
+                        currentTime: values[0],
+                      }))
+                    }
+                  }}
+                />
+                <p>{formatTime(playing.duration ?? 0)}</p>
+              </div>
+            </>
+          )}
         </div>
         <div className="size-full relative flex justify-end pr-8 items-center gap-x-6">
           <div className="absolute inset-0 -z-10 bg-gradient-to-l from-primary/10 to-transparent" />
@@ -490,3 +534,40 @@ const Gradient = ({ url }: { url?: string }) => {
     />
   )
 }
+
+const getShuffledSong = (() => {
+  let shuffledIndices: number[] = []
+  let currentIndex = songsStore.state.length
+
+  const shuffleIndices = () => {
+    shuffledIndices = Array.from(songsStore.state.keys())
+    for (let i = shuffledIndices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[shuffledIndices[i], shuffledIndices[j]] = [
+        shuffledIndices[j],
+        shuffledIndices[i],
+      ]
+    }
+    currentIndex = 0
+  }
+
+  return (currentSong: number) => {
+    if (songsStore.state.length === 1) {
+      return songsStore.state[0]
+    }
+
+    if (currentIndex >= shuffledIndices.length) {
+      shuffleIndices()
+    }
+
+    let nextIndex = shuffledIndices[currentIndex++]
+    while (nextIndex === currentSong && shuffledIndices.length > 1) {
+      if (currentIndex >= shuffledIndices.length) {
+        shuffleIndices()
+      }
+      nextIndex = shuffledIndices[currentIndex++]
+    }
+
+    return songsStore.state[nextIndex]
+  }
+})()
